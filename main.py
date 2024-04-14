8e6a5d9e7ad6211a2a1025b6b1387e128c19645b
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-14 08:46:52 -0500

    Refactor main.py to use Claude class for fetching entity properties and integrate with MongoDB

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, HTTPException;
-from pydantic import BaseModel;
 import requests;
-import utils.services.Claude as cladue;
+import json;
 
+from utils.classes.EcommerceCollection import EcommerceCollection
+from utils.classes.Message import Message;
+import utils.services.Claude as cladue;
+import utils.services.Mongo as mongo;
 
 app = FastAPI()
 claude = cladue.Claude()
+mongo = mongo.Mongo();
 
 
-class Message(BaseModel):
-    message: str
 
 @app.post("/message")
 async def create_message(message: Message):
@@ -17,8 +19,16 @@ async def create_message(message: Message):
     try:
         data = claude.get_message(message);       
         entity_properties = data['content'][0]['text'];
-        return entity_properties;
+        entity_properties_dict = json.loads(entity_properties);
+        action = entity_properties_dict["actions"];
+        print(entity_properties_dict);
+        newProducts = await mongo.get_data(action, entity_properties_dict);
+        print(newProducts);
+        responseData = claude.generate_response(message, action, newProducts);
+        finalMessage = responseData['content'][0]['text'];
+        return finalMessage;
 
     except requests.exceptions.RequestException as e:
         return HTTPException(status_code=500, detail=str(e));
-                
+
+
