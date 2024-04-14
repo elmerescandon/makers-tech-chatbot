795561aeb607a8915952d7cc0cc84fa7309b1ea4
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-14 08:47:07 -0500

    Refactor main.py to use Claude class for fetching entity properties and integrate with MongoDB

---
+++ utils/mockups.py
@@ -0,0 +1,375 @@
+
+laptop_mockup_1 = {
+    "id": "661b4c7da06298ebcedd7b36",
+    "category": "laptop",
+    "brand": "Microsoft",
+    "price": 1500,
+    "stock": 2,
+    "rating": 4.7,
+    "specs": {
+        "weight": 320,
+        "battery": "35"
+    },
+    "similar": ['Surface Laptop 4', 'Surface Book 3'],
+    "model": "Surface Laptop 3"
+}
+
+laptop_mockup_2 = {
+    "id": "661b4c7da06298ebcedd7b37",
+    "category": "laptop",
+    "brand": "Dell",
+    "price": 1200,
+    "stock": 5,
+    "rating": 4.2,
+    "specs": {
+        "weight": 350,
+        "battery": "40"
+    },
+    "similar": ['XPS 13', 'Inspiron 15'],
+    "model": "XPS 15"
+}
+
+laptop_mockup_3 = {
+    "id": "661b4c7da06298ebcedd7b38",
+    "category": "laptop",
+    "brand": "Lenovo",
+    "price": 800,
+    "stock": 10,
+    "rating": 4.0,
+    "specs": {
+        "weight": 400,
+        "battery": "50"
+    },
+    "similar": ['ThinkPad X1 Carbon', 'IdeaPad 5'],
+    "model": "ThinkPad T14"
+}
+
+laptop_mockup_4 = {
+    "id": "661b4c7da06298ebcedd7b39",
+    "category": "laptop",
+    "brand": "HP",
+    "price": 900,
+    "stock": 3,
+    "rating": 4.3,
+    "specs": {
+        "weight": 380,
+        "battery": "45"
+    },
+    "similar": ['Spectre x360', 'Envy 13'],
+    "model": "Pavilion 15"
+}
+
+products = [laptop_mockup_1, laptop_mockup_2, laptop_mockup_3, laptop_mockup_4]
+
+tablet_mockup_1 = {
+    "id": "661b4c7da06298ebcedd7b40",
+    "category": "tablet",
+    "brand": "Apple",
+    "price": 800,
+    "stock": 5,
+    "rating": 4.8,
+    "specs": {
+        "weight": 500,
+        "battery": "10"
+    },
+    "similar": ['iPad Pro', 'iPad Air'],
+    "model": "iPad Mini"
+}
+
+tablet_mockup_2 = {
+    "id": "661b4c7da06298ebcedd7b41",
+    "category": "tablet",
+    "brand": "Samsung",
+    "price": 700,
+    "stock": 8,
+    "rating": 4.5,
+    "specs": {
+        "weight": 450,
+        "battery": "12"
+    },
+    "similar": ['Galaxy Tab S7', 'Galaxy Tab A'],
+    "model": "Galaxy Tab S6"
+}
+
+tablet_mockup_3 = {
+    "id": "661b4c7da06298ebcedd7b42",
+    "category": "tablet",
+    "brand": "Microsoft",
+    "price": 1000,
+    "stock": 3,
+    "rating": 4.6,
+    "specs": {
+        "weight": 600,
+        "battery": "8"
+    },
+    "similar": ['Surface Pro 7', 'Surface Go'],
+    "model": "Surface Pro X"
+}
+
+tablet_mockup_4 = {
+    "id": "661b4c7da06298ebcedd7b43",
+    "category": "tablet",
+    "brand": "Lenovo",
+    "price": 600,
+    "stock": 6,
+    "rating": 4.2,
+    "specs": {
+        "weight": 550,
+        "battery": "9"
+    },
+    "similar": ['Yoga Tab 13', 'Tab P11 Pro'],
+    "model": "Tab M10"
+}
+
+tablet_mockup_5 = {
+    "id": "661b4c7da06298ebcedd7b44",
+    "category": "tablet",
+    "brand": "Huawei",
+    "price": 500,
+    "stock": 4,
+    "rating": 4.0,
+    "specs": {
+        "weight": 480,
+        "battery": "11"
+    },
+    "similar": ['MatePad Pro', 'MediaPad M5'],
+    "model": "MatePad 10"
+}
+
+tablets = [tablet_mockup_1, tablet_mockup_2, tablet_mockup_3, tablet_mockup_4, tablet_mockup_5]
+
+speaker_mockup_1 = {
+    "id": "661b4c7da06298ebcedd7b45",
+    "category": "speaker",
+    "brand": "Bose",
+    "price": 300,
+    "stock": 3,
+    "rating": 4.5,
+    "specs": {
+        "weight": 2,
+        "battery": "N/A"
+    },
+    "similar": ['SoundLink Revolve', 'SoundLink Mini II'],
+    "model": "SoundLink Color II"
+}
+
+speaker_mockup_2 = {
+    "id": "661b4c7da06298ebcedd7b46",
+    "category": "speaker",
+    "brand": "JBL",
+    "price": 200,
+    "stock": 5,
+    "rating": 4.2,
+    "specs": {
+        "weight": 1.5,
+        "battery": "N/A"
+    },
+    "similar": ['Charge 4', 'Flip 5'],
+    "model": "Clip 4"
+}
+
+speaker_mockup_3 = {
+    "id": "661b4c7da06298ebcedd7b47",
+    "category": "speaker",
+    "brand": "Sony",
+    "price": 250,
+    "stock": 4,
+    "rating": 4.4,
+    "specs": {
+        "weight": 2.5,
+        "battery": "N/A"
+    },
+    "similar": ['XB33', 'SRS-XB12'],
+    "model": "SRS-XB43"
+}
+
+speaker_mockup_4 = {
+    "id": "661b4c7da06298ebcedd7b48",
+    "category": "speaker",
+    "brand": "Harman Kardon",
+    "price": 350,
+    "stock": 2,
+    "rating": 4.7,
+    "specs": {
+        "weight": 3,
+        "battery": "N/A"
+    },
+    "similar": ['Onyx Studio 6', 'Go + Play'],
+    "model": "Citation 100"
+}
+
+speaker_mockup_5 = {
+    "id": "661b4c7da06298ebcedd7b49",
+    "category": "speaker",
+    "brand": "UE",
+    "price": 150,
+    "stock": 6,
+    "rating": 4.0,
+    "specs": {
+        "weight": 1,
+        "battery": "N/A"
+    },
+    "similar": ['Boom 3', 'Wonderboom 2'],
+    "model": "Megaboom 3"
+}
+
+speakers = [speaker_mockup_1, speaker_mockup_2, speaker_mockup_3, speaker_mockup_4, speaker_mockup_5]
+
+keyboard_mockup_1 = {
+    "id": "661b4c7da06298ebcedd7b50",
+    "category": "keyboard",
+    "brand": "Logitech",
+    "price": 100,
+    "stock": 10,
+    "rating": 4.5,
+    "specs": {
+        "type": "mechanical",
+        "layout": "QWERTY",
+        "backlight": True
+    },
+    "similar": ['G Pro X', 'G915'],
+    "model": "G513"
+}
+
+keyboard_mockup_2 = {
+    "id": "661b4c7da06298ebcedd7b51",
+    "category": "keyboard",
+    "brand": "Razer",
+    "price": 120,
+    "stock": 8,
+    "rating": 4.2,
+    "specs": {
+        "type": "mechanical",
+        "layout": "QWERTY",
+        "backlight": True
+    },
+    "similar": ['BlackWidow Elite', 'Huntsman'],
+    "model": "Cynosa Chroma"
+}
+
+keyboard_mockup_3 = {
+    "id": "661b4c7da06298ebcedd7b52",
+    "category": "keyboard",
+    "brand": "Corsair",
+    "price": 150,
+    "stock": 5,
+    "rating": 4.7,
+    "specs": {
+        "type": "mechanical",
+        "layout": "QWERTY",
+        "backlight": True
+    },
+    "similar": ['K70 RGB', 'K95 Platinum'],
+    "model": "K68"
+}
+
+keyboard_mockup_4 = {
+    "id": "661b4c7da06298ebcedd7b53",
+    "category": "keyboard",
+    "brand": "SteelSeries",
+    "price": 130,
+    "stock": 7,
+    "rating": 4.4,
+    "specs": {
+        "type": "mechanical",
+        "layout": "QWERTY",
+        "backlight": True
+    },
+    "similar": ['Apex Pro', 'Apex 7'],
+    "model": "Apex 5"
+}
+
+keyboard_mockup_5 = {
+    "id": "661b4c7da06298ebcedd7b54",
+    "category": "keyboard",
+    "brand": "HyperX",
+    "price": 90,
+    "stock": 12,
+    "rating": 4.0,
+    "specs": {
+        "type": "mechanical",
+        "layout": "QWERTY",
+        "backlight": True
+    },
+    "similar": ['Alloy Elite 2', 'Alloy Origins'],
+    "model": "Alloy FPS Pro"
+}
+
+keyboards = [keyboard_mockup_1, keyboard_mockup_2, keyboard_mockup_3, keyboard_mockup_4, keyboard_mockup_5]
+
+mouse_mockup_1 = {
+    "id": "661b4c7da06298ebcedd7b55",
+    "category": "mouse",
+    "brand": "Logitech",
+    "price": 50,
+    "stock": 10,
+    "rating": 4.5,
+    "specs": {
+        "type": "wired",
+        "dpi": 800
+    },
+    "similar": ['G502 Hero', 'G203 Lightsync'],
+    "model": "G305"
+}
+
+mouse_mockup_2 = {
+    "id": "661b4c7da06298ebcedd7b56",
+    "category": "mouse",
+    "brand": "Razer",
+    "price": 60,
+    "stock": 8,
+    "rating": 4.2,
+    "specs": {
+        "type": "wired",
+        "dpi": 16000
+    },
+    "similar": ['DeathAdder V2', 'Viper Mini'],
+    "model": "Basilisk X Hyperspeed"
+}
+
+mouse_mockup_3 = {
+    "id": "661b4c7da06298ebcedd7b57",
+    "category": "mouse",
+    "brand": "Corsair",
+    "price": 70,
+    "stock": 5,
+    "rating": 4.7,
+    "specs": {
+        "type": "wired",
+        "dpi": 18000
+    },
+    "similar": ['M65 RGB Elite', 'Harpoon RGB Pro'],
+    "model": "Ironclaw RGB"
+}
+
+mouse_mockup_4 = {
+    "id": "661b4c7da06298ebcedd7b58",
+    "category": "mouse",
+    "brand": "SteelSeries",
+    "price": 80,
+    "stock": 7,
+    "rating": 4.4,
+    "specs": {
+        "type": "wireless",
+        "dpi": 12000
+    },
+    "similar": ['Rival 650', 'Sensei Ten'],
+    "model": "Rival 3"
+}
+
+mouse_mockup_5 = {
+    "id": "661b4c7da06298ebcedd7b59",
+    "category": "mouse",
+    "brand": "HyperX",
+    "price": 90,
+    "stock": 12,
+    "rating": 4.0,
+    "specs": {
+        "type": "wireless",
+        "dpi": 16000
+    },
+    "similar": ['Pulsefire Dart', 'Pulsefire Haste'],
+    "model": "Pulsefire Surge"
+}
+
+mouses = [mouse_mockup_1, mouse_mockup_2, mouse_mockup_3, mouse_mockup_4, mouse_mockup_5]
